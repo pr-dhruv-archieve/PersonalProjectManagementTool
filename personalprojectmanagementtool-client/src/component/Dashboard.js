9eff9eb1c9bc69fdfc4f1744a2c8164d429f1c9c
pr-dhruv <pra_mahendra@hotmail.com>
2019-12-30 17:59:19 +0530

    Dashboard component added

---
+++ personalprojectmanagementtool-client/src/component/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <h1>Welcome to PPM Tool Dashboard</h1>
+        )
+    }
+}
+
+export default Dashboard;
