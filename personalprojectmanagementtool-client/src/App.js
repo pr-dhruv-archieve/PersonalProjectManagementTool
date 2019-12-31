d9b4bbed5a5d48f273b92070fce4cebfd509421f
Mahendra Prajapati <pra_mahendra@hotmail.com>
2019-12-31 14:41:15 +0530

    Navigation, Header and Layout components added

--- personalprojectmanagementtool-client/src/App.js
+++ personalprojectmanagementtool-client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './component/Dashboard';
+import Header from './component/Layout/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
