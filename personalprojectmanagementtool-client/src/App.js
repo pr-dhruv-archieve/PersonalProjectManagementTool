8d69f7772b94570e40d9537a5574fabb9ecd0f1d
pr-dhruv <pra_mahendra@hotmail.com>
2020-01-02 17:49:23 +0530

    Create Prject, Route component added

--- personalprojectmanagementtool-client/src/App.js
+++ personalprojectmanagementtool-client/src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Dashboard from './component/Dashboard';
 import Header from './component/Layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route} from 'react-router-dom'; // Creating 2 alias(Router and Route) for the BrowserRouter
+import AddProject from './component/Project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Route>
+      <div className="App">
+        <Header />
+        <Dashboard />
+        <Route path="/addProject" component={AddProject}/>
+      </div>
+    </Route>
   );
 }
 
---
+++ personalprojectmanagementtool-client/src/component/Project/AddProject.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddProject extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to add project form</h1>
+            </div>
+        )
+    }
+}
