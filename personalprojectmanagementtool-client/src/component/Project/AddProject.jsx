3646bb64348f11a7e7d1560a5f2abd5dcaf1825f
Mahendra Prajapati <pra_mahendra@hotmail.com>
2020-01-05 20:01:42 +0530

    name attribute added and values are according to Project Model in the back-end

--- personalprojectmanagementtool-client/src/component/Project/AddProject.jsx
+++ personalprojectmanagementtool-client/src/component/Project/AddProject.jsx
@@ -1,11 +1,71 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class AddProject extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Add project form</h1>
+  render() {
+    return (
+      <div>
+        {}
+
+        <div className="project">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h5 className="display-4 text-center">Create Project form</h5>
+                <hr />
+                <form>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg "
+                      placeholder="Project Name"
+                      name="projectName"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      placeholder="Unique Project ID"
+                      name="projectIdentifier"
+                    />
+                  </div>
+
+                  <div className="form-group">
+                    <textarea
+                      className="form-control form-control-lg"
+                      placeholder="Project Description"
+                      name="description"
+                    ></textarea>
+                  </div>
+                  <h6>Start Date</h6>
+                  <div className="form-group">
+                    <input
+                      type="date"
+                      className="form-control form-control-lg"
+                      name="start_date"
+                      name="startDate"
+                    />
+                  </div>
+                  <h6>Estimated End Date</h6>
+                  <div className="form-group">
+                    <input
+                      type="date"
+                      className="form-control form-control-lg"
+                      name="end_date"
+                      name="endDate"
+                    />
+                  </div>
+
+                  <input
+                    type="submit"
+                    className="btn btn-primary btn-block mt-4"
+                  />
+                </form>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
