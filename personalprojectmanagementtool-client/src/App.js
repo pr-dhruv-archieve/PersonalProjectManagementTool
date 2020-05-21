317bd29a43156c27b794616d58b71271c50736c7
Mahendra Prajapati <pra_mahendra@hotmail.com>
2020-05-21 13:50:31 +0530

    Provider tag added to App.js

--- personalprojectmanagementtool-client/src/App.js
+++ personalprojectmanagementtool-client/src/App.js
@@ -3,19 +3,22 @@ import './App.css';
 import Dashboard from './component/Dashboard';
 import Header from './component/Layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Route} from 'react-router-dom'; // Creating 2 alias(Router and Route) for the BrowserRouter
+import {BrowserRouter as Router, Route} from 'react-router-dom'; // Creating 2 alias(Router and Route) for the BrowserRouter
 import AddProject from './component/Project/AddProject';
+import {Provider} from 'react-redux';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/Dashboard" component={Dashboard}/>
-        <Route exact path="/addProject" component={AddProject}/>
-      </div>
-    </Router>
-  );
+    return (
+        <Provider>
+            <Router>
+                <div className="App">
+                    <Header/>
+                    <Route exact path="/Dashboard" component={Dashboard}/>
+                    <Route exact path="/addProject" component={AddProject}/>
+                </div>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
